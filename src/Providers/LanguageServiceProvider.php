854940e0c1de17414ad691cab67a291a08d14498
Abdelrahman Omran <me@omranic.com>
2024-07-24 17:07:51 +0400

    AddVoidReturnTypeWhereNoReturnRector

--- src/Providers/LanguageServiceProvider.php
+++ src/Providers/LanguageServiceProvider.php
@@ -12,7 +12,7 @@ class LanguageServiceProvider extends ServiceProvider
     /**
      * {@inheritdoc}
      */
-    public function boot()
+    public function boot(): void
     {
         // Add language validation rule
         Validator::extend('language', fn($attribute, $value): bool => is_string($value) && mb_strlen($value) === 2 && array_key_exists(mb_strtolower($value), languages()), __('validation.invalid_language'));
